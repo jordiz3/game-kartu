16bc9353a730f377623b8ead290e1d930b7fab69
Jordi TheZ <jordizahra60@gmail.com>
2025-07-07 16:15:48 +0000

    setelah saya tambahkan ke github, saya menerima email Please resolve the

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -2,11 +2,11 @@
 // You can find these details in your Firebase project settings.
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyC8UNkIKzaBesTpyGwbz2-BOJk-24qSEB8",
-  authDomain: "wishlist-date-app-1440c.firebaseapp.com",
-  projectId: "wishlist-date-app-1440c",
-  storageBucket: "wishlist-date-app-1440c.appspot.com",
-  messagingSenderId: "357859974192",
-  appId: "1:357859974192:web:e6311a2f8fe85068ace1f5",
-  measurementId: "G-QGJ4DRT0CE"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
