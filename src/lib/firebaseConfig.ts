cad23eba5e7d83d88d98b22bb45e276a1a3f9359
Jordi TheZ <jordizahra60@gmail.com>
2025-07-07 15:54:02 +0000

    tambhkan ke kode dan revisi ini config nya // For Firebase JS SDK v7.20.

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -2,10 +2,11 @@
 // You can find these details in your Firebase project settings.
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY", // Replace with your API key
-  authDomain: "YOUR_AUTH_DOMAIN", // Replace with your auth domain
-  projectId: "YOUR_PROJECT_ID", // Replace with your project ID
-  storageBucket: "YOUR_STORAGE_BUCKET", // Replace with your storage bucket
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID", // Replace with your messaging sender ID
-  appId: "YOUR_APP_ID", // Replace with your app ID
+  apiKey: "AIzaSyC8UNkIKzaBesTpyGwbz2-BOJk-24qSEB8",
+  authDomain: "wishlist-date-app-1440c.firebaseapp.com",
+  projectId: "wishlist-date-app-1440c",
+  storageBucket: "wishlist-date-app-1440c.appspot.com",
+  messagingSenderId: "357859974192",
+  appId: "1:357859974192:web:e6311a2f8fe85068ace1f5",
+  measurementId: "G-QGJ4DRT0CE"
 };
