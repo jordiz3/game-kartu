3118359a14f1f96903987317aa555775f6badfb1
Jordi TheZ <jordizahra60@gmail.com>
2025-07-10 05:13:47 +0000

    Pulling image: gcr.io/k8s-skaffold/pack Using default tag: latest latest

--- src/components/TruthOrDareGame.tsx
+++ src/components/TruthOrDareGame.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { cn } from '@/lib/utils';
+import { cn } from '../lib/utils';
 
 const truths = [
     "Kapan terakhir kali kamu bohong dan tentang apa?",
